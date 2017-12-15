54f5e737ab259479683cc7b09ce699cd344fe1db
alojua <juan.alonso@staempfli.com>
2017-12-15 17:44:05 +0100

    Add sample Jenkinsfile and some files adjustments

--- magento/app/code/Demo/Settings/registration.php
+++ magento/app/code/Demo/Settings/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'VendorName_ModuleName',
+    'Demo_Settings',
     __DIR__
 );
